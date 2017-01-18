9b0c467bdfdb86223eddb5d57508e7dc1e601103
raleigh wayland <rtwayland@gmail.com>
2017-01-18 15:07:05 -0700

    Complete

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,22 +1,43 @@
-angular.module('chatroom').controller('mainCtrl', function($scope, messageService){
+angular.module('chatroom').controller('mainCtrl', function($scope, messageService) {
 
-  //The getMessages function will call the getMessages method on the messageService object.
-  //You'll then save the result of that request to your controller's $scope as messages ($scope.messages)
-
-
+    //The getMessages function will call the getMessages method on the messageService object.
+    //You'll then save the result of that request to your controller's $scope as messages ($scope.messages)
 
 
-  //The postMessage function will take whatever the user typed in (hint: look at the html and see what ng-model correlates to on the input box),
-  //pass that text to the postMessage method on the messageService object which will
-  //then post it to the backend.
+    $scope.getMessages = function() {
+        messageService.getMessages()
+            .then(function(res) {
+                $scope.messages = res.data;
+                console.log($scope.messages);
+            }, function(error) {
+                console.log(error);
+            });
+    };
+
+    //The postMessage function will take whatever the user typed in (hint: look at the html and see what ng-model correlates to on the input box),
+    //pass that text to the postMessage method on the messageService object which will
+    //then post it to the backend.
+    $scope.postMessage = function() {
+        messageService.postMessage($scope.message);
+        $scope.message = '';
+    }
+
+    $scope.deleteMessages = function() {
+        // console.log('Message to delete:\n', theMessage);
+        messageService.deleteMessages()
+            .then(function(res) {
+                $scope.status = res.status;
+                console.log($scope.status);
+            }, function(error) {
+                console.log(error);
+            });
+    }
 
 
-
-  //uncomment this code when your getMessages function is finished
-  //This goes and gets new data every second, which mimicking a chat room experience.
-  //  setInterval(function(){
-    // $scope.getMessages();
-
-  // }, 1500)
+    //uncomment this code when your getMessages function is finished
+    //This goes and gets new data every second, which mimicking a chat room experience.
+    setInterval(function() {
+        $scope.getMessages();
+    }, 1500)
 
 })
